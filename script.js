f1be165a0593fca0e73a11c22e0ab3996885c5a3
Deniz Kelemence <dkelemence@gmail.com>
2021-03-23 13:49:38 +0000

    working on the project

--- script.js
+++ script.js
@@ -9,11 +9,13 @@
 
 
 //.....................................................................
+const rootElem = document.getElementById("root");
 
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
   showAllEpisodes(allEpisodes);
+  
 }
 
 function makePageForEpisodes(episodeList) {
@@ -23,54 +25,25 @@ function makePageForEpisodes(episodeList) {
 
 }
 
+function createCard(episode){
 
-function showAllEpisodes(episodes) {
-  const rootElem = document.getElementById("root");
-  let divContainerElement = document.createElement("div");
-  divContainerElement.className = "container";
-  let divRowElement = document.createElement("div");
-  divRowElement.className = "row";
-  for (let i = 0; i < episodes.length; i++) {
-    let episodeName = document.createElement("p");
-    let seasonNumber = document.createElement("p");
-    let episodeNumber = document.createElement("p");
-    let imageElement = document.createElement("img");
-    let summaryText = document.createElement("p");
-    episodeName = episodes[i].name;
-    seasonNumber = episodes[i].season.toString();
-    episodeNumber = episodes[i].number.toString();
-    // console.log(seasonNumber.length);
-    // console.log(episodeNumber.length);
-    imageElement = episodes[i].image.medium;
-    summaryText = episodes[i].summary;
+  let episodeName = document.createElement("p");
+  let seasonNumber = document.createElement("p");
+  let episodeNumber = document.createElement("p");
+  let imageElement = document.createElement("img");
+  let summaryText = document.createElement("p");
+  episodeName = episode.name;
+  seasonNumber = episode.season.toString();
+  episodeNumber = episode.number.toString();
 
-    // divRowElement.append(episodeName, seasonNumber, episodeNumber, imageElement, summaryText);
-    // if (10 < 20) {
-    // let boxModel =
-    //   `<div class="col-md-3">
-    //         <div class="episodeTitle">
-    //           <h4>${episodeName} S${seasonNumber}E${episodeNumber}</h4>
-    //         </div>
-    //         <div class="picture">
-    //         <img src= ${imageElement} alt="episode image">
-    //         </div>
-    //         <div class="sum">
-    //         <p>${summaryText}</p>
-    //         </div>
-    //       </div>`
-    // console.log(boxModel);
+  imageElement = episode.image.medium;
+  summaryText = episode.summary;
 
-    // divRowElement.innerHTML = divRowElement.innerHTML + boxModel;
-    // } else {
-    //   console.log("naaa");
-    // }
-    if ((seasonNumber.length > 1) && (episodeNumber.length > 1)) {
-      console.log("bad bug");
-      console.log(seasonNumber.length > 1 && episodeNumber.length > 1);
-      let boxModel =
-        `<div class="col-md-5th-1 col-sm-4">
+
+  let boxModel =
+    `<div class="col-md-5th-1 col-sm-4">
             <div class="episodeTitle">
-              <h4>${episodeName} S${seasonNumber}E${episodeNumber}</h4>
+              <h4>${episodeName} S${seasonNumber.padStart(2, "0")}E${episodeNumber.padStart(2, "0")}</h4>
             </div>
             <div class="picture">
             <img src= ${imageElement} alt="episode image">
@@ -79,60 +52,73 @@ function showAllEpisodes(episodes) {
             <p>${summaryText}</p>
             </div>
           </div>`
-      console.log(boxModel);
-       divRowElement.innerHTML = divRowElement.innerHTML + boxModel;
-      //--------------------------------------------------------------------------------------
-      //*************************STILL WORKING ON IT********************************************
-    } if (seasonNumber.length > 1 && episodeNumber.length == 1) {
-      console.log("bad bug");
-      let boxModel = `<div class="col-md-5th-1 col-sm-4">
-      <div class="episodeTitle">
-        <h4>${episodeName} S${seasonNumber}E0${episodeNumber}</h4>
-      </div>
-      <div class="picture">
-        <img src= ${imageElement} alt="episode image">
-    </div>
-        <div class="sum">
-          <p>${summaryText}</p>
-        </div>
-      </div>`
-       divRowElement.innerHTML = divRowElement.innerHTML + boxModel;
-    }
-    if (seasonNumber.length == 1 && episodeNumber.length > 1) {
-      console.log("bad bug");
-      let boxModel = `<div class="col-md-5th-1 col-sm-4">
-      <div class="episodeTitle">
-        <h4>${episodeName} S0${seasonNumber}E${episodeNumber}</h4>
-      </div>
-      <div class="picture">
-        <img src= ${imageElement} alt="episode image">
-    </div>
-        <div class="sum">
-          <p>${summaryText}</p>
-        </div>
-      </div>`
-       divRowElement.innerHTML = divRowElement.innerHTML + boxModel;
 
-    }
-    else {
-      let boxModel = `<div class="col-md-5th-1 col-sm-4">
-      <div class="episodeTitle">
-        <h4>${episodeName} S0${seasonNumber}E0${episodeNumber}</h4>
-      </div>
-      <div class="picture">
-        <img src= ${imageElement} alt="episode image">
-    </div>
-        <div class="sum">
-          <p>${summaryText}</p>
-        </div>
-      </div>`
-       divRowElement.innerHTML = divRowElement.innerHTML + boxModel;
-    }
-    //----------------------------------------------------------------------------------------
+  return boxModel;
+
+}
+
+function showAllEpisodes(episodes) {
+  let inputElement = document.createElement("input");
+  inputElement.setAttribute("type", "text");
+  document.body.insertBefore(inputElement, rootElem);
+  inputElement.addEventListener("keyup", () => {
+    // console.log(inputElement.value);
+    // Search box daki text ile eslesen episodelari bul
+    episodes.filter(episode => {
+      if (episode.name.includes(inputElement.value) || episode.summary.includes(inputElement.value)){
+      return true;  
+      } else {
+        return false;
+      } 
+    })
+    .forEach(episode => {
+    divRowElement.innerHTML = divRowElement.innerHTML + createCard(episode);
+    })
+    // let newArray = [];
+    // episodes.forEach(episode => {
+    //   if (episode.name.includes(inputElement.value) || episode.summary.includes(inputElement.value)) {
+    //     newArray.push(episode.id);
+    //   }
+    // })
+    // console.log(newArray);
+//eslesen episodlari karta donusturup listele
+
+  })
+
+
+  let divContainerElement = document.createElement("div");
+  divContainerElement.className = "container";
+  let divRowElement = document.createElement("div");
+  divRowElement.className = "row";
+  for (let i = 0; i < episodes.length; i++) {
+//  function created for the box model
+    divRowElement.innerHTML = divRowElement.innerHTML + createCard(episodes[i]);
   }
+
   rootElem.appendChild(divContainerElement);
   divContainerElement.appendChild(divRowElement);
 }
+
+
+// function searchBox(episodes) {
+//   let inputElement = document.createElement("input");
+//   inputElement.setAttribute("type", "text");
+//   // rootElem.appendChild(inputElement);
+//   document.body.insertBefore(inputElement, rootElem)
+//   inputElement.addEventListener("keyup", () => {
+//     console.log(inputElement.value);
+//     let newArray = [];
+//     episodes.forEach(episode => {
+//       if(episode.name.includes(inputElement.value) || episode.summary.includes(inputElement.value)){
+// newArray.push(episode.id);
+//       }
+//     })
+//     console.log(newArray);
+//     return newArray;
+
+//   })
+// }
+
 
 
 window.onload = setup;
