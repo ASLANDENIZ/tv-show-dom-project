d9ce3a9683f3f9f5e40b5d44714d8b75f9500c26
Deniz Kelemence <dkelemence@gmail.com>
2021-04-08 17:54:34 +0100

    working on it

--- script.js
+++ script.js
@@ -12,10 +12,12 @@ const rootElem = document.getElementById('root');
 let allEpisodes = [];
 let filteredEpisodes;
 let shows;
+
 function setup() {
   shows = getAllShows();
   // console.log('showss', shows);
   showAllShows(shows);
+  showAllEpisodes(shows, CreateShowsCard);
   // getAllEpisodesByFetch(`https://api.tvmaze.com/shows/82/episodes`).then(
   //   (episodes) => {
   //     allEpisodes = [...episodes];
@@ -24,7 +26,7 @@ function setup() {
   //     episodeList(allEpisodes);
   //   }
   // );
-  
+
 }
 window.onload = setup;
 //this function fetch show
@@ -52,10 +54,10 @@ showSelectElement.id = 'display-shows';
 showLabelElement.appendChild(showSelectElement);
 //this functions create options for shows
 function showAllShows(shows) {
-  let sortedShows = shows.sort(function(a,b){
+  let sortedShows = shows.sort(function (a, b) {
     var nameA = a.name.toLowerCase();
     var nameB = b.name.toLowerCase();
-    if(nameA < nameB) {
+    if (nameA < nameB) {
       return -1;
     } else if (nameA > nameB) {
       return 1;
@@ -91,7 +93,7 @@ selectShowEl.addEventListener('change', (e) => {
     allEpisodes = [...episodes];
     totalEpisodes = allEpisodes;
     makePageForEpisodes(allEpisodes);
-    showAllEpisodes(allEpisodes);
+    showAllEpisodes(allEpisodes, createCard);
     episodeList(allEpisodes);
   });
 });
@@ -145,15 +147,92 @@ function createCard(episode) {
           </div>`;
   return card;
 }
+function CreateShowsCard(show) {
+  // When your app starts, present a listing of all shows("shows listing")
+  // For each show, you must display at least name, image, summary, genres, status, rating, and runtime.
+  // When a show name is clicked, your app should:
+  // fetch and present episodes from that show(enabling episode search and selection as before)
+  // hide the "shows listing" view.
+  // Add a navigation link to enable the user to return to the "shows listing"
+  // When this is clicked, the episodes listing should be hidden
+  // Provide a free - text show search through show names, genres, and summary texts.
+  // Ensure that your episode search and episode selector controls still work correctly when you switch from shows listing to episodes listing and back.
+  let showName = document.createElement("p");
+  let showSummary = document.createElement("p");
+  let showRated = document.createElement("p");
+  let showGenres = document.createElement("p");
+  let showStatus = document.createElement("p");
+  let showRuntime = document.createElement("p");
+  let showImage = document.createElement("img");
+  showName = show.name;
+  showSummary = show.summary;
+  showRated = show.rating.average;
+  showGenres = show.genres;
+  showStatus = show.status;
+  showRuntime = show.runtime;
+
+  // showImage = show.image.medium;
+  if (showImage === null) {
+    let card = `<div class="col-md-5th-1 col-sm-4">
+      <div class="showName">
+        <h2>${showName}</h2>
+      </div>
+      <div class="show-image">
+        <img src= ${image ? show.image.medium : } alt='Show Image'>
+      </div>
+      <div class="show-summary">
+        <p>${showSummary}</p>
+      </div>
+      <div class="show-details">
+        <p><span>Genres:</span> ${showGenres}</p>
+        <p><span>Status:</span> ${showStatus}</p>
+        <p><span>Runtime:</span> ${showRuntime}</p>
+      </div>
+    </div> `
+    return card;
+  } else {
+    let card = `<div class="col-md-5th-1 col-sm-4">
+      <div class="showName">
+        <h2>${showName}</h2>
+      </div>
+      <div class="show-image">
+        <img src=${showImage} alt="Show Image">
+      </div>
+      <div class="show-summary">
+        <p>${showSummary}</p>
+      </div>
+      <div class="show-details">
+        <p><span>Genres</span>${showGenres}</p>
+        <p><span>Status</span>${showStatus}</p>
+        <p><span>Runtime</span>${showRuntime}</p>
+      </div>
+    </div> `
+
+    return card;
+  }
+  
+  
+
+}
+
+
+
+
+console.log(shows);
+
+
+
+
+
 // this function creates BootsTrap elements and show each episode card on the screen
-function showAllEpisodes(episodes) {
+function showAllEpisodes(episodes, cardFunction) {
   let divContainerElement = document.createElement('div');
   divContainerElement.className = 'container';
   let divRowElement = document.createElement('div');
   divRowElement.className = 'row';
   for (let i = 0; i < episodes.length; i++) {
     //  function created for the box model
-    divRowElement.innerHTML = divRowElement.innerHTML + createCard(episodes[i]);
+    divRowElement.innerHTML = divRowElement.innerHTML + cardFunction(episodes[i]);
   }
   rootElem.appendChild(divContainerElement);
   divContainerElement.appendChild(divRowElement);
@@ -179,7 +258,7 @@ function episodeList(episodes) {
     let episodeNames = episode.name;
     let seasonNumbers = episode.season.toString();
     let episodeNumbers = episode.number.toString();
-    let uniqueCode = (optionEpisodesElement.innerHTML = `S${seasonNumbers.padStart(2,'0')}E${episodeNumbers.padStart(2, '0')} ${episodeNames}`);
+    let uniqueCode = (optionEpisodesElement.innerHTML = `S${seasonNumbers.padStart(2, '0')}E${episodeNumbers.padStart(2, '0')} ${episodeNames}`);
     // creates a unique code for each episode
     episode.uniqueCodeNumber = uniqueCode;
   });
@@ -196,7 +275,7 @@ selectElement.addEventListener('change', (e) => {
   divRowElement = document.querySelector('.row');
   divRowElement.parentNode.removeChild(divRowElement);
   makePageForEpisodes(filteredEpisodes);
-  showAllEpisodes(filteredEpisodes);
+  showAllEpisodes(filteredEpisodes, createCard);
 });
 //here we create search bar
 let searchBarElement = document.createElement('input');
@@ -216,8 +295,9 @@ searchBarElement.addEventListener('keyup', (e) => {
   divRowElement = document.querySelector('.row');
   divRowElement.parentNode.removeChild(divRowElement);
   makePageForEpisodes(filteredEpisodes);
-  showAllEpisodes(filteredEpisodes);
+  showAllEpisodes(filteredEpisodes, createCard);
 });
+
 
 
 
